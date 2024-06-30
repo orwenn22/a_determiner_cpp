ac18ad0fbe4e7bd9b875bec4ce0bff8c1eaab95b
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2024-06-30 18:36:30 +0200

    FileOp : Save u32 in little endian form + add imhex hexpat for lev format

--- src/utils/FileOp.cpp
+++ src/utils/FileOp.cpp
@@ -10,18 +10,18 @@ static_assert(sizeof(float) == 4, "float type must be 4 bytes");
 #endif
 
 void WriteU32(uint32_t val, FILE *f) {
-    fputc((int)((val >> 24) & 0xff), f);
-    fputc((int)((val >> 16) & 0xff), f);
-    fputc((int)((val >>  8) & 0xff), f);
     fputc((int)( val        & 0xff), f);
+    fputc((int)((val >>  8) & 0xff), f);
+    fputc((int)((val >> 16) & 0xff), f);
+    fputc((int)((val >> 24) & 0xff), f);
 }
 
 uint32_t ReadU32(FILE *f) {
     uint32_t r = 0;
-    r |= (fgetc(f) & 0xff) << 24;
-    r |= (fgetc(f) & 0xff) << 16;
-    r |= (fgetc(f) & 0xff) <<  8;
     r |= (fgetc(f) & 0xff);
+    r |= (fgetc(f) & 0xff) <<  8;
+    r |= (fgetc(f) & 0xff) << 16;
+    r |= (fgetc(f) & 0xff) << 24;
     return r;
 }
 
